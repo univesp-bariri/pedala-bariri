25e0288152b111c4a89f7ba64e538f2d5bc36cdf
univesp-bariri <univesp.bariri@gmail.com>
2021-11-07 21:27:32 -0300

    Teste

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash
+from flask_mail import Mail, Message
+
 app = Flask('__name__')
+app.secret_key = "itsok"
+
+email_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": GMAIL_USER,
+    "MAIL_PASSWORD": GMAIL_PASSWORD
+}
+
+app.config.update(email_settings)
+mail = Mail(app)
+
+class Contato:
+    def __init__(self,nome,email,mensagem):
+        self.nome = nome
+        self.email = email
+        self.mensagem = mensagem
 
 @app.route('/')
 def index():
